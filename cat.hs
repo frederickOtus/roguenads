a91b43d09c127c353b046897fa2f9e2d64ccfec5
peter <pita.da.bread07@gmail.com>
2016-02-19 23:38:50 -0500

    I need to figure out types in haskell before i can accomplish anything

--- cat.hs
+++ cat.hs
@@ -1,12 +1,16 @@
+type Symbol = (Char, Int, Int)
+type Token = (String, Int, Int)
 
-indexChars :: [Char] -> [(Char, Int, Int)]
+indexChars :: [Char] -> [Symbol]
 indexChars c = rindexChars c 0 0
 
-rindexChars :: [Char] -> Int ->  Int -> [(Char, Int, Int)]
+rindexChars :: [Char] -> Int ->  Int -> [Symbol]
 rindexChars [] _ _ = []
 rindexChars (c:cs) r col
     | c == '\n' = (c, r, col) : (rindexChars cs (r + 1) 0)
     | otherwise = (c, r, col) : (rindexChars cs r (col + 1))
+
+delims = "\n\t ()"
 
 main = fmap (show . indexChars) fstring >>= putStrLn
         where fstring = readFile "test"
