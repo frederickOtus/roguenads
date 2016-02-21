9403162da2ea69d0a4f0eaa26976bb27872a3c76
peter <pita.da.bread07@gmail.com>
2016-02-20 22:40:12 -0500

    applicative functors were kind of a dead end. I have no idea what i'm doing

--- cat.hs
+++ cat.hs
@@ -1,16 +1,35 @@
-type Symbol = (Char, Int, Int)
-type Token = (String, Int, Int)
+import Control.Applicative
 
-indexChars :: [Char] -> [Symbol]
-indexChars c = rindexChars c 0 0
+newtype Indexed a = Indexed {getSym :: (a, Int, Int) }
+    deriving Show
 
-rindexChars :: [Char] -> Int ->  Int -> [Symbol]
-rindexChars [] _ _ = []
-rindexChars (c:cs) r col
-    | c == '\n' = (c, r, col) : (rindexChars cs (r + 1) 0)
-    | otherwise = (c, r, col) : (rindexChars cs r (col + 1))
+instance Functor Indexed where
+    fmap f (Indexed (x, r, c)) = Indexed ((f x), r, c)
 
-delims = "\n\t ()"
+instance Applicative Indexed where
+    pure x = Indexed (x, 0, 0)
+    (Indexed (x, r, c)) <*> ind = fmap x ind
 
-main = fmap (show . indexChars) fstring >>= putStrLn
-        where fstring = readFile "test"
+type Symbol = Indexed Char
+type Token = Indexed String
+
+a = Indexed ('a', 0, 0)
+b = Indexed ('b', 0, 1)
+l = Indexed ([], 0, 0) :: Indexed [Char]
+
+aa = Indexed ("a", 0, 0)
+bb = Indexed ("b", 0, 0)
+
+--indexChars :: [Char] -> [Symbol]
+--indexChars c = rindexChars c 0 0
+
+--rindexChars :: [Char] -> Int ->  Int -> [Symbol]
+--rindexChars [] _ _ = []
+--rindexChars (c:cs) r col
+--    | c == '\n' = (c, r, col) : (rindexChars cs (r + 1) 0)
+--    | otherwise = (c, r, col) : (rindexChars cs r (col + 1))
+
+--delims = "\n\t ()"
+
+--main = fmap (show . indexChars) fstring >>= putStrLn
+--        where fstring = readFile "test"
