3b272922669d9c6b49d8b8cea8867a5480955249
peter <pita.da.bread07@gmail.com>
2016-02-22 16:26:18 -0500

    'tokenizes'

--- cat.hs
+++ cat.hs
@@ -27,6 +27,41 @@ addSymb :: Symbol -> Token -> Token
 addSymb (Indexed (c, _)) t = fmap (strv++) t
     where strv = pure c
 
+ind = Index {row=0, col=0}
+exDelims = " \t\n"
+inDelims = "()"
+
+has :: Eq a => [a] -> a -> Bool
+has l elm = foldr (||) False $ map (==elm) l
+
+baseToken :: Symbol -> Token
+baseToken (Indexed (c,i)) = Indexed ([c],i)
+
+chunkWith :: ([a] -> ([a],[a])) -> [a] -> [[a]]
+chunkWith _ [] = []
+chunkWith f xs = let (h,t) = f xs
+                    in h : chunkWith f t
+
+symChunker :: [Symbol] -> ([Symbol],[Symbol])
+symChunker [] = ([],[])
+symChunker a@(s@(Indexed (c, i)):ss)
+    | has inDelims c = ([s],ss)
+    | otherwise = rsymChunker a
+
+rsymChunker :: [Symbol] -> ([Symbol],[Symbol])
+rsymChunker [] = ([],[])
+rsymChunker (s@(Indexed (c, i)):ss)
+    | has exDelims c = ([],ss)
+    | has inDelims c = ([],s:ss)
+    | otherwise = let (syms, rest) = rsymChunker ss
+                    in (s:syms, rest)
+
+chunkSyms = chunkWith symChunker
+tokenize :: [Symbol] -> [Token]
+tokenize syms = map toTokens chnkdSyms
+    where chnkdSyms = chunkSyms syms
+          toTokens = foldr addSymb (newInd "" 0 0 :: Token)
+
 newInd a r c = Indexed (a, Index {row = r, col = c})
 
 indexChars :: [Char] -> [Symbol]
@@ -38,7 +73,6 @@ rindexChars (c:cs) r col
     | c == '\n' = newInd c r col : (rindexChars cs (r + 1) 0)
     | otherwise = newInd c r col : (rindexChars cs r (col + 1))
 
-delims = "\n\t ()"
-
-main = fmap (show . indexChars) fstring >>= putStrLn
+main = fmap (show . filt . tokenize . indexChars) fstring >>= putStrLn
         where fstring = readFile "test"
+              filt = filter (\(Indexed (s, _)) -> s /= [])
