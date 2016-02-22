60463fcaa7acbb9d77411c2ddc7e6339dfd21e14
peter <pita.da.bread07@gmail.com>
2016-02-21 21:15:01 -0500

    abandoned applicatives, because I am not good at this

--- cat.hs
+++ cat.hs
@@ -1,35 +1,44 @@
 import Control.Applicative
 
-newtype Indexed a = Indexed {getSym :: (a, Int, Int) }
+data Index = Index { row::Int, col::Int }
+    deriving Show
+
+instance Eq Index where
+    Index {row = a, col = b} == Index {row = c, col = d} = a == c && b == d
+
+instance Ord Index where
+    Index {row = a, col = b} `compare` Index {row = c, col = d}
+        | a > c = GT
+        | a < c = LT
+        | b > d = GT
+        | b < d = LT
+        | otherwise = EQ
+
+newtype Indexed a = Indexed (a, Index)
     deriving Show
 
 instance Functor Indexed where
-    fmap f (Indexed (x, r, c)) = Indexed ((f x), r, c)
-
-instance Applicative Indexed where
-    pure x = Indexed (x, 0, 0)
-    (Indexed (x, r, c)) <*> ind = fmap x ind
+    fmap f (Indexed (a, i)) = Indexed (f a, i)
 
 type Symbol = Indexed Char
 type Token = Indexed String
 
-a = Indexed ('a', 0, 0)
-b = Indexed ('b', 0, 1)
-l = Indexed ([], 0, 0) :: Indexed [Char]
+addSymb :: Symbol -> Token -> Token
+addSymb (Indexed (c, _)) t = fmap (strv++) t
+    where strv = pure c
 
-aa = Indexed ("a", 0, 0)
-bb = Indexed ("b", 0, 0)
+newInd a r c = Indexed (a, Index {row = r, col = c})
 
---indexChars :: [Char] -> [Symbol]
---indexChars c = rindexChars c 0 0
+indexChars :: [Char] -> [Symbol]
+indexChars c = rindexChars c 0 0
 
---rindexChars :: [Char] -> Int ->  Int -> [Symbol]
---rindexChars [] _ _ = []
---rindexChars (c:cs) r col
---    | c == '\n' = (c, r, col) : (rindexChars cs (r + 1) 0)
---    | otherwise = (c, r, col) : (rindexChars cs r (col + 1))
+rindexChars :: [Char] -> Int ->  Int -> [Symbol]
+rindexChars [] _ _ = []
+rindexChars (c:cs) r col
+    | c == '\n' = newInd c r col : (rindexChars cs (r + 1) 0)
+    | otherwise = newInd c r col : (rindexChars cs r (col + 1))
 
---delims = "\n\t ()"
+delims = "\n\t ()"
 
---main = fmap (show . indexChars) fstring >>= putStrLn
---        where fstring = readFile "test"
+main = fmap (show . indexChars) fstring >>= putStrLn
+        where fstring = readFile "test"
